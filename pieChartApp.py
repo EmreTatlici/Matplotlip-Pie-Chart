fb2543eaaf9ee93c070291d69663ea9cba978bc7
EmreTatlici <92151030+EmreTatlici@users.noreply.github.com>
2022-06-24 16:12:55 +0300

    Update pieChartApp.py

--- pieChartApp.py
+++ pieChartApp.py
@@ -1,29 +1,29 @@
 import matplotlib.pyplot as plt
 
-# meyvelerin olsun, kullanici meyve alsin, ve buna bagli olarak satilan meyve oranlarini pasta grafigi uzerinde gosteriniz.
+# you have fruits, users take them, and you can show the rate of selling of each fruits in the pie chart.
 
-alinan_meyveler = [0,0,0,0,0]
+taken_fruits = [0,0,0,0,0]
 
-meyveler = ['elma','armut','muz','cilek','seftali']
+fruits = ['elma','armut','muz','cilek','seftali']
 
 
-def meyveAl(meyveAdi):
-    for meyve in meyveler:
-        if meyveAdi == meyve:
-            indeks = meyveler.index(meyveAdi)
-            alinan_meyveler[indeks] += 1
+def buyFruit(fruitName):
+    for fruit in fruits:
+        if fruitName == fruit:
+            index = fruits.index(fruitName)
+            taken_fruits[index] += 1
 
 
 while True:
-    islem = input('1- meyve satin al\n2- pie chart goster\n3- cikis yap\n')
+    option = input('1- buy fruit \n2- show pie chart\n3- exit\n')
 
-    if islem == '1':
-        meyveAdi = input('meyve adi gir: ')
-        meyveAl(meyveAdi)
+    if option == '1':
+        fruitName = input('name of fruit you want to buy: ')
+        buyFruit(fruitName)
 
 
     elif islem == '2':
-        pieChart = plt.pie(alinan_meyveler, labels= meyveler,autopct='%1.1f%%')
+        pieChart = plt.pie(taken_fruits, labels= fruits,autopct='%1.1f%%')
         # explode = myexplode
         plt.legend()
         plt.show()
@@ -31,23 +31,7 @@ while True:
     elif islem == '3':
         break
     else:
-        print('hatali tuslama!!')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print('wrong option!!')
 
 
 
